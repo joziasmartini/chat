af132283a93e89d1eff6b2e35d7f6f156dff0a24
Jozias Martini <jozias.martini@gmail.com>
2023-09-05 14:02:20 -0300

    Create client side with page for messages (#3)
    * feat: create page for messages
    
    * refactor: fix port and binary messages on server

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 
-const port = 7070;
+const port = 7171;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(data) {
+  ws.on("message", function incoming(data, isBinary) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
+        client.send(data, { binary: isBinary });
       }
     });
   });
